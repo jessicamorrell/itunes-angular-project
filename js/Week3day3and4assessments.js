81d4992aaf67c76a3f1d38994ee78d531da15c88
Jessica Morrell <jessica.ann.morrell@gmail.com>
2016-02-20 20:12:57 -0700

    finishing up step 4

---
+++ js/Week3day3and4assessments.js
@@ -0,0 +1,27 @@
+// 1 choice D
+
+//2 choice B
+
+//3 choice A $http.get('/someURL')
+
+//4 promise 
+
+//5 this
+
+//6 service
+
+// 7 method
+
+
+//DAY 4
+//1 A provides a way to execute asynchronous functions
+
+//2 promise
+
+//3 B
+
+//4 $q
+
+//5 resolve
+
+//6 defer
